9bf33c6eac8c901c0a84096631ea7386a4fc2b16
NouCake <KevinFischer2010@gmail.com>
2019-05-11 13:02:59 +0200

    PSE proto

---
+++ Tst/Assets/Room.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+
+    private Transform Wall_L;
+    private Transform Wall_R;
+    private Transform Wall_F;
+    private Transform Wall_B;
+    private Transform Roof;
+
+    void Start() {
+        Wall_L = transform.Find("Wall_L");
+        Wall_R = transform.Find("Wall_R");
+        Wall_F = transform.Find("Wall_F");
+        Wall_B = transform.Find("Wall_B");
+        Roof = transform.Find("Roof");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnTriggerStay(Collider other) {
+        Debug.Log("Hello World!");
+        if(other.tag == "Player")
+        {
+            Wall_F.gameObject.SetActive(false);
+            Roof.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Wall_F.gameObject.SetActive(true);
+        Roof.gameObject.SetActive(true);
+    }
+}
---
+++ Tst/Assets/Ship.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    public float Speed = 1;
+    public float RotSpeed = 180;
+    public float ShootSpeed = 10;
+    public float ShotsPerSecond = 5;
+    private float timer;
+
+    public Transform ship;
+    private Rigidbody body;
+    public GameObject shoot;
+
+    void Start() {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKey(KeyCode.A)) {
+            ship.transform.localEulerAngles += Vector3.up * RotSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            ship.transform.localEulerAngles -= Vector3.up * RotSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            body.velocity = ship.transform.forward * Speed;
+        } else {
+            body.velocity = Vector3.zero;
+        }
+
+
+        if (Input.GetKey(KeyCode.Return)) {
+            if(timer > 0) {
+                timer -= Time.deltaTime;
+            } else {
+                GameObject shot = Instantiate(shoot);
+                shot.transform.position = transform.position;
+                shot.GetComponent<Rigidbody>().velocity = ship.forward * ShootSpeed;
+                timer = 1 / ShotsPerSecond;
+            }
+        }
+    }
+}
